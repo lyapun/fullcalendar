7caf0a0f46bc5b2760e211e525174075667f5f1f
Taras Lyapun <taraslyapun@gmail.com>
2015-06-10 17:51:10 +0300

    Commented striping zone code

--- src/EventManager.js
+++ src/EventManager.js
@@ -851,16 +851,16 @@ function EventManager(options) { // assumed to be a calendar
 
 			// if the dates have changed, and we know it is impossible to recompute the
 			// timezone offsets, strip the zone.
-			if (
-				isAmbigTimezone &&
-				!newProps.allDay &&
-				(dateDelta || durationDelta)
-			) {
-				newProps.start.stripZone();
-				if (newProps.end) {
-					newProps.end.stripZone();
-				}
-			}
+			// if (
+			// 	isAmbigTimezone &&
+			// 	!newProps.allDay &&
+			// 	(dateDelta || durationDelta)
+			// ) {
+			// 	newProps.start.stripZone();
+			// 	if (newProps.end) {
+			// 		newProps.end.stripZone();
+			// 	}
+			// }
 
 			$.extend(event, miscProps, newProps); // copy over misc props, then date-related props
 			backupEventDates(event); // regenerate internal _start/_end/_allDay
